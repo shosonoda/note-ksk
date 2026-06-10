9bda19222d1580ae1fee7ace369043127e824a1a
shosonoda <sho.sonoda@riken.jp>
2026-06-10 21:22:28 +0900

    init aitp

--- NoteKsk/Basic.lean
+++ NoteKsk/Basic.lean
@@ -1,1 +1,14 @@
+/-
+Copyright (c) 2026 Sho Sonoda. All rights reserved.
+Released under MIT license as described in the file LICENSE.
+Authors: Sho Sonoda
+-/
+import Mathlib
+
+/-!
+`x ^ 2 ≥ 0`
+-/
+theorem sq_nonneg_example (x : ℝ) : x ^ 2 ≥ 0 := by
+  exact sq_nonneg x
+
 def hello := "world"
